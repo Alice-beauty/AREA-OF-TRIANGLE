ff648d431884c4ac74d1b9ed47215c6fdb4aa991
Alice-beauty <115928236+Alice-beauty@users.noreply.github.com>
2022-11-11 17:30:21 +0300

    Add files via upload

---
+++ assignment II.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //variable declaration
+    int base;
+    int height;
+    int hypotenuse;
+    int area;
+    
+    //variable initialization
+    base=10;
+    height=7;
+    hypotenuse=12;
+    
+    //logic
+    area=0.5 * base * height;
+    
+    //output
+    cout<<"my area is: ";
+    cout<<area;
+}
